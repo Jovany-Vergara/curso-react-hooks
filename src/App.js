6b31795cbd5b0bbf862d45752110ce60c7493665
Jovany Vergara <jovany@makingdevs.com>
2022-05-17 14:42:15 -0500

    [master] import characters and themeContext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Characters from './components/Characters';
+import ThemeContext from './context/ThemeContext';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Characters />
       <h1>Hola mundo</h1>
     </div>
   );
